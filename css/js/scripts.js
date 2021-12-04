6f937751924e40c26849955184201c372fb0b1ae
Madhavi <angelmadhavi17@gmail.com>
2021-12-04 15:08:14 -0500

    before _submit

--- css/js/scripts.js
+++ css/js/scripts.js
@@ -1,4 +1,9 @@
-function enlargeimg(){
-    img = document.getElementById("womanimg7");
-    img.style.transform = "scale(1.5)";
-}
+{/* <script type="text/javascript"> */}
+            function myFunction(){
+                    img = document.getElementById("womanimg7").innerHTML;
+                    // alert("hello");
+                    // img.style.transform = "scale(1.5)";
+                    // img.style.transition = "transform 0.25s ease";
+                    img.style.width = "400px";
+                }
+// </script>
